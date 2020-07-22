fb8b7a92630e38daa8287489b0316d0a9ec2ebc7
Nataliia Dyshko <nataliia.dyshko@ukr.net>
2020-07-22 13:38:45 +0300

    Move some things to init for better structure

--- app/__init__.py
+++ app/__init__.py
@@ -4,19 +4,28 @@ from whitenoise import WhiteNoise
 from flask_migrate import Migrate
 
 from .models.database import db, base
-from .views.manage_blueprints import users, add_users_routes
+from .views import users_bp, add_users_routes
+
+
+migrate = Migrate()
+jwt = JWTManager()
 
 
 def setup_database(app):
-    with app.app_context():  # CAN WE DO THAT???
+    with app.app_context():  # WHAT IS THAT FOR???
         @app.before_first_request
         def create_tables():
             base.metadata.create_all(db)
 
+    migrate.init_app(app, db)
+
 
 def setup_jwt(app):
-    jwt = JWTManager(app)
+    jwt.init_app(app)
 
+    # CAN THIS BE MOVED TO revoked_token_model?
+    # WHEN SHOULD IT BE DECORATED AND WHEN IT WILL BE
+    # IF IN revoked_token_model?
     from .models.revoked_token_model import RevokedTokenModel
 
     @jwt.token_in_blacklist_loader
@@ -40,10 +49,6 @@ def create_app():
 
     # adding resources. Do that AFTER setup_database()
     add_users_routes()  # creates it's own api and adds it there
-    app.register_blueprint(users)  # blueprint connects that api and app
+    app.register_blueprint(users_bp)  # blueprint connects that api and app
 
-    from .models.revoked_token_model import RevokedTokenModel
-    from .models.user_model import UserModel
-
-    migrate = Migrate(app, db)
     return app
--- app/views/users.py
+++ app/views/users.py
@@ -3,10 +3,9 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import (create_access_token, create_refresh_token,
     jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 
-
-from ..models.user_model import UserModel
-from ..models.revoked_token_model import RevokedTokenModel
-from ..models.user_logging_model import UserLoggingModel
+from app.models.user_model import UserModel
+from app.models.revoked_token_model import RevokedTokenModel
+from app.models.user_logging_model import UserLoggingModel
 
 
 parser = reqparse.RequestParser()
