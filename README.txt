3c19476e32deea4b7b18ac4f3b5b7c1b7dd07eb5
Nataliia Dyshko <nataliia.dyshko@jetsoftpro.com>
2020-11-04 10:34:29 +0200

    Update config.py and readme.txt for github

--- README.txt
+++ README.txt
@@ -1,21 +1,12 @@
+Small apllication that requires postgresql, celery and redis.
+Uses JWT tokens, uses db migrations via alembic and can send emails async because of celery:)
+
 Has NO cyclic imports! 
 Application Factory pattern was used to avoid it.
-(You can see how pretty run.py is now)
-
-Used to be run like this:
-FLASK_APP=run.py FLASK_DEBUG=1 flask run
-(runs even with cyclic imports)
-
-And like this:
-python run.py
-
-Uses postgreSQL, use cmd to select tables.
-
-You better have config files outside your app package.
-Config.py is imported in run.py create_app()
 
 Uses gunicorn for WSGI support,
 and WhiteNoise for static file serving.
+
 
 NOW run like this (you'll need different tabs):
 sudo service redis-server start
@@ -26,3 +17,10 @@ To play run Postman requests.
 
 Don't forget to stop redis afterwards (just to be neat):
 sudo service redis-server stop
+
+P.S.
+You better have config files outside your app package.
+Config.py is imported in run.py create_app()
+This config.py is an example and requires your data for app to run.
+
+FOR MORE INFO READ COMMIT MESSAGES:)
--- config.py
+++ config.py
@@ -1,20 +1,14 @@
-# import os
-# basedir = os.path.abspath(os.path.dirname(__file__))
-# ?
-
-
 class Config(object):
     DEBUG = False
     TESTING = False
-    CSRF_ENABLED = True  # ?
+    CSRF_ENABLED = True
     SECRET_KEY = 'aaa'
 
-    JWT_SECRET_KEY = 'jwt-secret-key'
+    JWT_SECRET_KEY = 'your-jwt-secret-key'
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
-    #  not used yet
     SQLALCHEMY_DATABASE_URL = \
-        "postgres://nataliia:nat_postgres_88@localhost:5432/nataliia"
+        "postgres://abc:your_pass@localhost:5432/db_name"
 
     CELERY_BROKER_URL = 'redis://localhost:6379/0'
     CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
@@ -22,11 +16,10 @@ class Config(object):
     # Flask-Mail configuration
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465 #587
-    #MAIL_USE_TLS = True
     MAIL_USE_SSL = True
 
-    MAIL_USERNAME = 'nataliia.dyshko@gmail.com'
-    MAIL_PASSWORD = '555678gmail'
+    MAIL_USERNAME = 'your_email@example.com'
+    MAIL_PASSWORD = 'your_pass'
     MAIL_DEFAULT_SENDER = 'flask@example.com'
 
 
